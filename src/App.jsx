6bba4696f96cbeceb9a21eb53b6fa9ee9923be53
Ben Bishop <ben.bishop@purplebricks.com>
2019-08-23 12:22:51 +1000

    Added context hook, fetched the items from the API and consumed the items in <SHOP>

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Router } from "@reach/router";
 
 import Header from "./components/Header/Header";
 import Shop from "./pages/Shop/Shop";
 import Cart from "./pages/Cart/Cart";
 
+import ShopContext from "./context/ShopContext";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState();
+
+  useEffect(() => {
+    fetch(
+      "https://wooliesxfechallenge.azurewebsites.net/api/v1/resources/products"
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setItems(data);
+      });
+  });
+
   return (
     <div className="app">
-      <Header />
-      <div className="container">
-        <Router>
-          <Shop path="/" />
-          <Cart path="/cart" />
-        </Router>
-      </div>
+      <ShopContext.Provider value={{ items }}>
+        <Header />
+        <div className="container">
+          <Router>
+            <Shop path="/" />
+            <Cart path="/cart" />
+          </Router>
+        </div>
+      </ShopContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import ShopContext from "../../context/ShopContext";
 
 const Shop = () => {
-  return <h1>Shop</h1>;
+  const { items } = useContext(ShopContext);
+
+  return (
+    <div>
+      <h1>Shop</h1>
+      {items && <h2>items loaded</h2>}
+    </div>
+  );
 };
 
 export default Shop;
